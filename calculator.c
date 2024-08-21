b031e50ac9c64d7b77f4001d2a214fb89a9291db
iddo <iddosadeh@gmail.com>
2024-08-21 16:22:08 -0700

    Initial commit

---
+++ calculator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+double add(double a, double b) {
+    return a + b;
+}
+
+double subtract(double a, double b) {
+    return a - b;
+}
+
+double multiply(double a, double b) {
+    return a * b;
+}
+
+double divide(double a, double b) {
+    if (b != 0)
+        return a / b;
+    else {
+        return 0;  // We'll handle division by zero in JavaScript
+    }
+}
+
